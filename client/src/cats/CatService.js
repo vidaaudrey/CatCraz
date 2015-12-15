0b7f5f795a6dcfe81d3b78c1cab3a70b9fd94844
vidaaudrey <audrey@33road.com>
2015-12-14 18:49:06 -0800

    Got relational mongo db working, restified users, cats and categories

--- client/src/cats/CatService.js
+++ client/src/cats/CatService.js
@@ -18,18 +18,6 @@
         url: 'http://24.media.tumblr.com/Jjkybd3nSc0hubrh9sB4zSFU_500.jpg', 
         avatar: 'svg-2',
         content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
-      },
-      {
-        name: 'Gener Delosreyes',
-        url: 'http://25.media.tumblr.com/tumblr_kox053z0jL1qzvqipo1_400.gif', 
-        avatar: 'svg-3',
-        content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
-      },
-      {
-        name: 'Lawrence Ray',
-        url: 'http://25.media.tumblr.com/tumblr_lkxhd01k9f1qiq7lto1_500.gif', 
-        avatar: 'svg-4',
-        content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
       }
     ];
 
--- server/server.js
+++ server/server.js
@@ -4,13 +4,15 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var _ = require('lodash');
 var restful = require('node-restful');
-
+var Schema = mongoose.Schema;
 
 // Create the application.
 var app = express();
 app.use(express.static('client'));
 // Add Middleware necessary for REST API's
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
@@ -23,23 +25,228 @@ app.use(function(req, res, next) {
 });
 
 mongoose.connect('mongodb://audrey:audrey@ds029595.mongolab.com:29595/catcraz')
-.connection.once('open', function() {
-  console.log('Connected to mongolab, and listening on port 3000...');
-  app.listen(3000);
+  .connection.once('open', function() {
+    console.log('Connected to mongolab, and listening on port 3000...');
+    app.listen(3000);
+  });
+
+var UserSchema = new Schema({
+  username: String,
+  avatar: String
 });
+
+var CategorySchema = new Schema({
+  name: String
+});
+
+var KatSchema = new Schema({
+  title: String,
+  url: {
+    type: String
+  },
+  vote: {
+    type: Number,
+    default: 0
+  },
+  fans: [UserSchema],
+  categories: [CategorySchema],
+  comments: [{
+    text: {
+      type: String,
+      required: true
+    },
+    user: UserSchema
+  }],
+  modified: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+var Kat = app.kat = restful.model('kat', KatSchema).methods(['get', 'post', 'put', 'delete']);
+var User = app.user = restful.model('user', UserSchema).methods(['get', 'post', 'put', 'delete']);
+var Category = app.category = restful.model('categories', CategorySchema).methods(['get', 'post', 'put', 'delete']);
+
+
+Kat.register(app, '/cats');
+User.register(app, '/users');
+Category.register(app, '/categories');
+// var UserSchema = new Schema({
+//   username: String,
+//   avatar: String
+// });
+// var CategorySchema = new Schema({
+//   name: String
+// });
+
+// var CommentSchema = new Schema({
+//   text: { type: String, required: true}, 
+//   user: UserSchema
+// });
+
+// var KatSchema = new Schema({
+//   title: String,
+//   url: {
+//     type: String
+//   },
+//   vote: {
+//     type: Number,
+//     default: 0
+//   },
+//   fans: [UserSchema],
+//   categories: [CategorySchema],
+//   comments: [{
+//       text: {
+//         type: String,
+//         required: true
+//       },
+//       user: UserSchema
+//     }], 
+//     modified: { type: Date, default: Date.now }
+// });
+
+// var User = mongoose.model('cats', UserSchema);
+// var Category = mongoose.model('categories', CategorySchema);
+// var Comment = mongoose.model('comments', CommentSchema);
+// var Kat = mongoose.model('kats', KatSchema);
+// var newUser = new User({
+//   username: 'Audrey'
+// });
+// var newUser1 = new User({
+//   username: 'JJ'
+// });
+// newUser.save(function(err, user) {
+//   console.log('After saving user online', err, user);
+// });
+
+// newUser1.save(function(err, user) {
+//   console.log('After saving user online', err, user);
+// });
+// var newCategory = new Category({
+//   name: "Popular"
+// });
+// newCategory.save(function(err, cat) {
+//   console.log('category saved', err, cat);
+// });
+
+// var newComment = new Comment({
+//     text: "Cool", 
+//     user: newUser
+// });
+// newComment.save(function(err, comment){
+//     console.log('comment saved', err, comment);
+// });
+
+// var newKat = new Kat({
+//   title: "Cool lulu",
+//   url: "http://29.media.tumblr.com/tumblr_ly4pyyiH8U1qh66wqo1_1280.jpg",
+//   fans: [newUser, newUser1],
+//   categories: [newCategory],
+//   comments: [{
+//       text: "Cool",
+//       user: newUser
+//     }, {
+//       text: "Cute cat!",
+//       user: newUser1
+//     }
+//   ]
+// });
+// newKat.save(function(err, kat) {
+//   console.log('kat saved', err, kat);
+// });
+
+
+
+
+// User.register(app, '/users');
+
+// var Cat = app.cat = restful.model('cat', mongoose.Schema({
+//     title: String,
+//     url: String,
+//     vote: 0,
+//     fans: {
+//       text: String,
+//       username: String,
+//       avatar: String
+//     }
+//   }))
+//   .methods(['get', 'post', 'put', 'delete']);
+
+// Cat.register(app, '/cats');
+
+
+
+// app.get('/cats', function(req, res, next) {
+//   Kat.find({}, function(err, cats) {
+//     res.end('ok', cats);
+//   });
+// });
+
+// db.cat.insert({title: "The King", url: "http://29.media.tumblr.com/tumblr_ly4pyyiH8U1qh66wqo1_1280.jpg", vote: 0,  fans: [{text: "Cute cata!", username: "Audrey", avatar: "svg-1"}, {username: "Otto", avatar: "svg-2"}], content: "Something to fill later"})
+
+
+// var User = app.user = restful.model('user', mongoose.Schema({
+//     name: String
+//   }))
+//   .methods(['get', 'post', 'put', 'delete']);
+
+
+// var Cat = app.cat = restful.model('cat', mongoose.Schema({
+//     title: String,
+//     url: String,
+//     vote: 0,
+//     fans: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'User'
+//     },
+//     comments: {
+//       text: String,
+//       postedBy: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User'
+//       }
+//     }
+//   }))
+//   .methods(['get', 'post', 'put', 'delete']);
+
+// User.register(app, '/users');
+
+
+
+// curl -XPOST "http://localhost:3000/cats/" -d'
+// {
+//    "title": "The Matrix",
+//    "url": "http://29.media.tumblr.com/tumblr_ly4pyyiH8U1qh66wqo1_1280.jpg", 
+//    "avatar": "svg-1", 
+//    "content": "something", 
+//    "fans": [
+//     {"type": "566f518092bb6853c7631fe1"}
+//     ], 
+//     "comments": [
+//       {"text": "Cute cat!", "postBy": "566f518092bb6853c7631fe1"},
+//       {"text": "Not cool!", "postBy": "566f518092bb6853c7631fe1"}
+//     ] 
+// }'
+
+
+// add a new cat record 
+// curl -d '{"name": "King", "url": "http://29.media.tumblr.com/tumblr_ly4pyyiH8U1qh66wqo1_1280.jpg", "avatar": "svg-1", "content": "something", "fans": [{"type": "566f518092bb6853c7631fe1"}], "comments": [{"text": "Cute cat!", "postBy": "566f518092bb6853c7631fe1"}, {"text": "Not cool!", "postBy": "566f518092bb6853c7631fe1"}] }' -H "Content-Type:application/json" http://localhost:3000/cats/ 
+
+
+//curl -d '{"name": "King", "url": "http://29.media.tumblr.com/tumblr_ly4pyyiH8U1qh66wqo1_1280.jpg", "avatar": "svg-1", "content": "something", "fans": {"type": "566f518092bb6853c7631fe1"} }' -H "Content-Type: application/json" http://localhost:3000/cats/ 
 
 
 // curl -i http://localhost:3000/cats/
 // curl -d '{"title": "King", "year": "1932"}' -H "Content-Type:application/json" http://localhost:3000/cats/ -X DELETE
 
-var Cat = app.cat = restful.model('cat', mongoose.Schema({
-    title: String,
-    url: String, 
-    vote: 0
-  }))
-  .methods(['get', 'post', 'put', 'delete']);
+// curl -i http://localhost:3000/users/
+// curl -d '{"username": "Audrey"}' -H "Content-Type:application/json" http://localhost:3000/users/ 
+// {"__v":0,"_id":"566f518092bb6853c7631fe1"}%  
 
-Cat.register(app, '/cats');
+// delete cat by id
+// curl -d '{"_id":"566f518092bb6853c7631fe1" }' -H "Content-Type:application/json" http://localhost:3000/cats/ -X DELETE
+
 
 
 // app.get('/',  function(req, res){
