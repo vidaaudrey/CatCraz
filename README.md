40a689e9b7119597a774cac181367f75aed1ee7d
vidaaudrey <audrey@33road.com>
2016-01-10 13:57:31 -0800

    fix: add github link

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Angular, Angular-Material, NodeJS, Node-Restful, Express, MongoDB, Heroku
 ### Demo 
 1. visit http://CatBook100.heroku.com 
 
-### Developing
+### Develop
 
 1. Run `npm install` to install server dependencies.
 
@@ -17,14 +17,17 @@ Angular, Angular-Material, NodeJS, Node-Restful, Express, MongoDB, Heroku
 
 3. Run `grunt` to start the development server.
 
-## TODO (may not finish)
+### Deploy
+Push to heroku or connect Heroku with github repo, and create a mongolab add-on under Heroku resources
+
+### TODO (may not finish)
 - [ ] Add cat 
 - [ ] Edit cat 
 - [ ] Share cat 
 - [ ] Add more cat picture  
 
 
-## Author  
+### Author  
    [Audrey Li](http://audreyli.me) 
 
 
--- client/src/cats/CatController.js
+++ client/src/cats/CatController.js
@@ -1,15 +1,15 @@
-(function(){
+(function () {
 
   angular
-       .module('cats')
-       .controller('CatController', [
-          'catService', '$mdSidenav', '$mdBottomSheet', '$log', '$q', '$mdDialog', '$mdMedia', 
-          CatController
-       ]);
-       // .controller('CommentFormCtrl', [
-       //    'catService',
-       //    CommentFormCtrl
-       // ]);
+    .module('cats')
+    .controller('CatController', [
+      'catService', '$mdSidenav', '$mdBottomSheet', '$log', '$q', '$mdDialog', '$mdMedia',
+      CatController
+    ]);
+  // .controller('CommentFormCtrl', [
+  //    'catService',
+  //    CommentFormCtrl
+  // ]);
 
 
 
@@ -20,76 +20,76 @@
    * @param avatarsService
    * @constructor
    */
-  function CatController( catService, $mdSidenav, $mdBottomSheet, $log, $q, $mdDialog, $mdMedia) {
+  function CatController(catService, $mdSidenav, $mdBottomSheet, $log, $q, $mdDialog, $mdMedia) {
     var self = this;
 
-    self.selected     = null;
-    self.cats        = [ ];
-    self.selectCat   = selectCat;
-    self.toggleList   = toggleCatsList;
-    self.showContactOptions  = showContactOptions;
+    self.selected = null;
+    self.cats = [];
+    self.selectCat = selectCat;
+    self.toggleList = toggleCatsList;
+    self.showContactOptions = showContactOptions;
     self.showAlert = showAlert;
     self.getCommentsHeaderString = getCommentsHeaderString;
     self.voteUp = voteUp;
     self.deleteCat = deleteCat;
     self.submitComment = submitComment;
     self.newComment = {
-          username: 'John Doe',
-          avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/maz/128.jpg',
-          text: 'I want to say...'
+      username: 'John Doe',
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/maz/128.jpg',
+      text: 'I want to say...'
     };
 
     // Load all registered cats
 
     catService
-          .loadAllCats()
-          .then( function( cats ) {
-            console.log('got cats from service', cats);
-            self.cats    = [].concat(cats);
-            self.selected = cats[0];
-          });
+      .loadAllCats()
+      .then(function (cats) {
+        console.log('got cats from service', cats);
+        self.cats = [].concat(cats);
+        self.selected = cats[0];
+      });
 
     // *********************************
     // Internal methods
     // *********************************
 
-    function submitComment(ev){
+    function submitComment(ev) {
       console.log('submitting new comment', self.newComment);
       self.selected.comments.push(self.newComment);
       // reset
       self.newComment = {
-            username: 'John Doe',
-            avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/maz/128.jpg',
-            text: 'I want to say...'
+        username: 'John Doe',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/maz/128.jpg',
+        text: 'I want to say...'
       };
       catService
         .saveVotes(self.selected)
-        .then(function(savedCat){
+        .then(function (savedCat) {
           console.log('Cat new comment saved to server', savedCat.comments);
         });
 
     }
 
-    function voteUp(ev){
+    function voteUp(ev) {
       self.selected.vote++;
       console.log(self.selected.vote);
       catService
-         .saveVotes(self.selected)
-        .then(function(savedCat){
+        .saveVotes(self.selected)
+        .then(function (savedCat) {
           console.log('Cat vote saved to server', savedCat.vote);
         });
     }
 
-    function deleteCat(ev){
+    function deleteCat(ev) {
       console.log("trying to delete cat", self.selected);
       var index = self.cats.indexOf(self.selected);
       catService
-         .deleteCat(self.selected)
-        .then(function(msg){
+        .deleteCat(self.selected)
+        .then(function (msg) {
           console.log('Cat deleted from server', msg);
           // automatically move to the next cat in the view
           var nextIndex = _getNextCat(index);
-          if(nextIndex !== null){
+          if (nextIndex !== null) {
             self.selectCat(self.cats[nextIndex]);
             self.cats.splice(index, 1);
             console.log('selecting next cat');
@@ -97,29 +97,29 @@
         });
     }
 
-    function _getNextCat(index){
-      if(self.cats === null)  return null;
-      if(index >= self.cats.length - 1 || index < 0) {
+    function _getNextCat(index) {
+      if (self.cats === null) return null;
+      if (index >= self.cats.length - 1 || index < 0) {
         return 0;
       }
-      return index+1;
+      return index + 1;
     }
 
     function showAlert(ev) {
-        // Appending dialog to document.body to cover sidenav in docs app
-        // Modal dialogs should fully cover application
-        // to prevent interaction outside of dialog
-        $mdDialog.show(
-          $mdDialog.alert()
-            .parent(angular.element(document.querySelector('#popupContainer')))
-            .clickOutsideToClose(true)
-            .title('About CatBook')
-            .textContent('CatBook, formerly known as CatCraz, is an app built for crazy cat lovers.   CatBook helps you get out of boredom, greatlly reduce your stress level, and ultimately improve your mental and physical health.   ///// - Created with love by Audrey')
-            .ariaLabel('About CatBook Dialog')
-            .ok('Got it.')
-            .targetEvent(ev)
-        );
-      }
+      // Appending dialog to document.body to cover sidenav in docs app
+      // Modal dialogs should fully cover application
+      // to prevent interaction outside of dialog
+      $mdDialog.show(
+        $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title('About CatBook')
+        .textContent('CatBook, formerly known as CatCraz, is an app built for crazy cat lovers.   CatBook helps you get out of boredom, greatlly reduce your stress level, and ultimately improve your mental and physical health.   ///// - Created with love by Audrey (Github: VidaAudrey)')
+        .ariaLabel('About CatBook Dialog')
+        .ok('Got it.')
+        .targetEvent(ev)
+      );
+    }
 
     /**
      * First hide the bottomsheet IF visible, then
@@ -128,7 +128,7 @@
     function toggleCatsList() {
       var pending = $mdBottomSheet.hide() || $q.when(true);
 
-      pending.then(function(){
+      pending.then(function () {
         $mdSidenav('left').toggle();
       });
     }
@@ -137,50 +137,61 @@
      * Select the current avatars
      * @param menuId
      */
-    function selectCat ( cat ) {
+    function selectCat(cat) {
       self.selected = angular.isNumber(cat) ? $scope.cats[cat] : cat;
       self.toggleList();
     }
 
-    function getCommentsHeaderString(comments){
-      if(!comments) return '';
-      if(comments.length === 0 ) return "No comments"; 
-      if(comments.length === 1 ) return "One comment"; 
-      return  comments.length + " comments"; 
+    function getCommentsHeaderString(comments) {
+      if (!comments) return '';
+      if (comments.length === 0) return "No comments";
+      if (comments.length === 1) return "One comment";
+      return comments.length + " comments";
     }
 
     /**
      * Show the bottom sheet
      */
     function showContactOptions($event) {
-        var cat = self.selected;
+      var cat = self.selected;
 
-        return $mdBottomSheet.show({
-          parent: angular.element(document.getElementById('content')),
-          templateUrl: './src/cats/view/contactSheet.html',
-          controller: [ '$mdBottomSheet', ContactPanelController],
-          controllerAs: "cp",
-          bindToController : true,
-          targetEvent: $event
-        }).then(function(clickedItem) {
-          clickedItem && $log.debug( clickedItem.name + ' clicked!');
-        });
+      return $mdBottomSheet.show({
+        parent: angular.element(document.getElementById('content')),
+        templateUrl: './src/cats/view/contactSheet.html',
+        controller: ['$mdBottomSheet', ContactPanelController],
+        controllerAs: "cp",
+        bindToController: true,
+        targetEvent: $event
+      }).then(function (clickedItem) {
+        clickedItem && $log.debug(clickedItem.name + ' clicked!');
+      });
 
-        /**
-         * Bottom Sheet controller for the Avatar Actions
-         */
-        function ContactPanelController( $mdBottomSheet ) {
-          this.cat = cat;
-          this.actions = [
-            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-          ];
-          this.submitContact = function(action) {
-            $mdBottomSheet.hide(action);
-          };
-        }
+      /**
+       * Bottom Sheet controller for the Avatar Actions
+       */
+      function ContactPanelController($mdBottomSheet) {
+        this.cat = cat;
+        this.actions = [{
+          name: 'Phone',
+          icon: 'phone',
+          icon_url: 'assets/svg/phone.svg'
+        }, {
+          name: 'Twitter',
+          icon: 'twitter',
+          icon_url: 'assets/svg/twitter.svg'
+        }, {
+          name: 'Google+',
+          icon: 'google_plus',
+          icon_url: 'assets/svg/google_plus.svg'
+        }, {
+          name: 'Hangout',
+          icon: 'hangouts',
+          icon_url: 'assets/svg/hangouts.svg'
+        }];
+        this.submitContact = function (action) {
+          $mdBottomSheet.hide(action);
+        };
+      }
     }
 
   }
